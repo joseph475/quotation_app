f575822de047c3730974c386ab0950e4cb731ef8
joseph475 <mjc@newswatch.dk>
2023-12-20 22:40:27 +0800

    nxt-2 commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,24 @@
-import { h } from 'preact';
+import { h, Component, render } from 'preact';
 import {
   MagnifyingGlassIcon,
 } from '@heroicons/react/24/outline'
 
-const Search = ({ data, setSearchResults, searchColumns, searchPlaceHolder = "Search..." }) => {
-  const handleSearch = (event) => {
+class SearchComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchQuery: '',
+    };
+
+    this.searchInput = null;
+  }
+
+  handleSearchChange = (event) => {
+    const {data, searchColumns, setSearchResults } = this.props
+
+    this.setState({ searchQuery: event.target.value });
+
     const searchTerm = event.target.value.toLowerCase();
     const filteredData = data.filter((item) =>
       searchColumns.some(
@@ -14,22 +28,33 @@ const Search = ({ data, setSearchResults, searchColumns, searchPlaceHolder = "Se
     setSearchResults(filteredData);
   };
 
-  return (
-    <div class="bg-white dark:bg-gray-900">
-      <label for="table-search" class="sr-only">Search</label>
-      <div class="relative mt-1">
-        <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
-          <MagnifyingGlassIcon className="h-4 w-4 text-blue-500" />
+  clearSearch = () => {
+    const {data, searchColumns, setSearchResults } = this.props;
+    this.setState({ searchQuery: '' });
+    setSearchResults(data);
+  };
+
+  render({ searchPlaceHolder }) {
+    return (
+      <div class="bg-white dark:bg-gray-900">
+        <label for="table-search" class="sr-only">Search</label>
+        <div class="relative mt-1">
+          <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+            <MagnifyingGlassIcon className="h-4 w-4 text-blue-500" />
+          </div>
+          <input
+            type="text"
+            id="table-search"
+            value={this.state.searchQuery}
+            onInput={this.handleSearchChange}
+            placeholder={searchPlaceHolder}
+            ref={(input) => (this.searchInput = input)}
+            class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
         </div>
-        <input 
-          type="text" 
-          id="table-search"
-          placeholder= {searchPlaceHolder}
-          onKeyUp={handleSearch}
-          class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-        />
       </div>
-    </div>
-  );
+    );
+  }
 }
-export default Search;
+
+export default SearchComponent;
--- src/pages/ConfigPages/Classifications.jsx
+++ src/pages/ConfigPages/Classifications.jsx
@@ -1,13 +1,9 @@
 import { h, Component, render } from 'preact';
-// import { API_ENDPOINTS } from '../../config/apiConfig';
 import {
   fetchDataFromAPI,
   fetchLocalStorage,
   storeData
 } from '../../../helpers';
-import {
-  PencilSquareIcon
-} from '@heroicons/react/24/outline'
 import Search from '../../components/Search';
 
 class Classifications extends Component {
@@ -17,6 +13,7 @@ class Classifications extends Component {
     this.state = {
       filteredItems: [],
       editingCell: null,
+      searchQuery: '',
       exludedColumns: [
         'id',
         'created_at',
@@ -28,12 +25,19 @@ class Classifications extends Component {
     };
 
     this.inputRef = null;
+    this.searchComponentRef = null;
   }
 
   setSearchResults = (results) => {
     this.setState({
       filteredItems: results
     });
+  };
+
+  handleClearSearch = () => {
+    if (this.searchComponentRef) {
+      this.searchComponentRef.clearSearch();
+    }
   };
 
   handleDoubleClick = (rowIndex, colName) => {
@@ -80,13 +84,10 @@ class Classifications extends Component {
     });
   }
 
-  handleBlur = () => {
-    this.onClassficationUpdate()
-  };
-
   handleEnterKeyPress = (e) => {
     if (e.key === 'Enter') {
       this.onClassficationUpdate()
+      this.handleClearSearch()
     }
   };
 
@@ -141,9 +142,10 @@ class Classifications extends Component {
               setSearchResults={this.setSearchResults}
               searchColumns={searchColumns}
               searchPlaceHolder="Add classification here"
+              ref={(ref) => (this.searchComponentRef = ref)}
             />
           </div>
-          <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Save</button>
+          <button onClick={this.handleClearSearch} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Save</button>
         </form>
 
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 table-auto shadow-md">
@@ -176,7 +178,10 @@ class Classifications extends Component {
                             type="text"
                             value={item[colName]}
                             onChange={this.handleChange}
-                            onBlur={this.handleBlur}
+                            onBlur={() =>{ this.setState({
+                              editingCell: null
+                            })}}
+                            onKeyDown={this.handleEnterKeyPress}
                             ref={(input) => (this.inputRef = input)}
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                           />
